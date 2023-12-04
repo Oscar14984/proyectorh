55ccac3894d9c023d9f37f76fa3237ea90691b76
pedro-morales-a <p007bon@gmail.com>
2023-12-04 12:03:19 -0600

    update semaforo fecha limite

--- src/Vacantes/VacantesColombia.svelte
+++ src/Vacantes/VacantesColombia.svelte
@@ -45,13 +45,34 @@ const getPuestos = async () =>{
     const res = await axios.post(Lugar.backend + 'getPuestosData.php')
     const data = JSON.parse(res.data.d);
     // if(tienePuestos){
-      jsonSalida = Object.values(data.jsonSalida);
+      jsonSalida = Object.values(data.jsonSalida).map(puesto => {
+        const fechaLimite = new Date(puesto.fecha_limite);
+        const fechaActual = new Date();
+
+        // Calcular la diferencia en milisegundos
+        const diferenciaMilisegundos = fechaLimite - fechaActual;
+
+        // Calcular la diferencia en días
+        const diferenciaDias = Math.floor(diferenciaMilisegundos / (1000 * 60 * 60 * 24));
+
+        return { ...puesto, diferenciaDias };
+      });
       console.log(jsonSalida)
     // } 
   } catch (error) {
     
   }
 };
+// Función para determinar la clase CSS basada en la diferencia de días
+const getFecha = (diferenciaDias) => {
+    if (diferenciaDias >= 15) {
+      return 'verde';  // Clase CSS para color verde
+    } else if (diferenciaDias >= 0) {
+      return 'amarillo';  // Clase CSS para color amarillo
+    } else {
+      return 'rojo';  // Clase CSS para color rojo
+    }
+  }
   onMount(() => {
     getPuestos();
   });
@@ -64,7 +85,7 @@ const getPuestos = async () =>{
   <div class="container">
     <!-- {#if tienePuestos} -->
       {#each jsonSalida as puesto (puesto.id_puesto)}
-      <div class="card mt-3">
+      <div class="card mt-3 {getFecha(puesto.diferenciaDias)}">
         <div class="card-body">
           <h5 class="card-title">{puesto.titulo}</h5>
           <p class="card-text"><strong>Descripción:</strong> {puesto.descripcion}</p>
@@ -106,3 +127,9 @@ const getPuestos = async () =>{
     {/if} -->
   </div>
 </main>
+
+<style>
+  .verde { background-color: green; }
+  .amarillo { background-color: yellow; }
+  .rojo { background-color: red; }
+</style>
