756c9b0f1ebe26264f794f2ea4db18f1e3f2627e
PMoralesAlvarez <p007bon@gmail.com>
2023-12-08 17:46:57 -0600

    update

--- src/Vacantes/VacantePrueba.svelte
+++ src/Vacantes/VacantePrueba.svelte
@@ -1,115 +1,176 @@
 <script>
-    // @ts-nocheck
-      import Spinner from './../Componentes/Spinner.svelte';
-    // @ts-nocheck
-    import axios from "axios";
-    import Lugar from "../Lugares";
-    import { session } from "../session";
-    import { onMount } from 'svelte';
+// LIBRERIAS O COMPONENTES CON VARIABLE EXTRA
+// @ts-nocheck
+import Spinner from './../Componentes/Spinner.svelte';
+let spinner = false;
+
+// LIBRERIAS O COMPONENTES SIN VARIABLE EXTRA
+// @ts-nocheck
+import axios from "axios";
+import Lugar from "../Lugares";
+import { session } from "../session";
+import { onMount } from 'svelte';
+import Modal from '../Componentes/Modal.svelte';
+
   
+    
+//Para exportar el id_usuario cuando hace inicio de sesión.
+let idUsuario = null;
+idUsuario = $session.id_usuario;
+console.log(idUsuario)
   
-    let spinner = false;
-    let idUsuario = null;
-  //Para exportar el id_usuario cuando hace inicio de sesión.
-    idUsuario = $session.id_usuario;
-    console.log(idUsuario)
-  
-  // let tienePuestos;
-  let jsonSalida = [];
-  const getPuestos = async () =>{
-    try {
-      spinner = true
-      const res = await axios.post(Lugar.backend + 'getPuestosData.php')
-      const data = JSON.parse(res.data.d);
-      spinner = false
-        jsonSalida = Object.values(data.jsonSalida).map(puesto => {
-          const fechaLimite = new Date(puesto.fecha_limite);
-          const fechaActual = new Date();
-          const idPuesto = puesto.id_puesto;
-          console.log(idPuesto)
-  
-          postularse(idPuesto);
-          // Calcular la diferencia en milisegundos
-          // @ts-ignore
-          const diferenciaMilisegundos = fechaLimite - fechaActual;
-  
-          // Calcular la diferencia en días
-          const diferenciaDias = Math.floor(diferenciaMilisegundos / (1000 * 60 * 60 * 24));
-  
-  
-          return { ...puesto, diferenciaDias };
-          // tienePuestos = jsonSalida.length > 0;
-  
-        });
-        console.log(jsonSalida)
-      
+// let tienePuestos;
+let jsonSalida = [];
+const getPuestos = async () =>{
+try {
+    spinner = true
+    const res = await axios.post(Lugar.backend + 'getPuestosData.php')
+    const data = JSON.parse(res.data.d);
+    spinner = false
+    jsonSalida = Object.values(data.jsonSalida).map(puesto => {
+        const fechaLimite = new Date(puesto.fecha_limite);
+        const fechaActual = new Date();
+        const idPuesto = puesto.id_puesto;
+        console.log(idPuesto)
+
+        postularse(idPuesto);
+        // Calcular la diferencia en milisegundos
+        // @ts-ignore
+        const diferenciaMilisegundos = fechaLimite - fechaActual;
+
+        // Calcular la diferencia en días
+        const diferenciaDias = Math.floor(diferenciaMilisegundos / (1000 * 60 * 60 * 24));
+
+
+        return { ...puesto, diferenciaDias };
+        // tienePuestos = jsonSalida.length > 0;
+
+    });
+    console.log(jsonSalida)
+    
     } catch (error) {
-      
+    
     }
-  };
-  // Función para determinar la clase CSS basada en la diferencia de días
-  const getFecha = (diferenciaDias) => {
-      if (diferenciaDias >= 15) {
+};
+// Función para determinar la clase CSS basada en la diferencia de días
+const getFecha = (diferenciaDias) => {
+    if (diferenciaDias >= 15) {
         return 'verde';  // Clase CSS para color verde
-      } else if (diferenciaDias >= 0) {
+        } else if (diferenciaDias >= 0) {
         return 'amarillo';  // Clase CSS para color amarillo
-      } else {
+        } else {
         return 'rojo';  // Clase CSS para color rojo
-      }
+        }
     }
     onMount(() => {
-      getPuestos();
-    });
-  
-    //para postutularse
-    const postularse = async (idPuesto) =>{
-      try {
-        const res = await axios.post(Lugar.backend+'postulacion.php',{
-          id_puesto:idPuesto,
-          // @ts-ignore
-          id_usuario:idUsuario,
-        })
-      } catch (error) {
-        
-      };
+    getPuestos();
+});
+
+//para postutularse
+const postularse = async (idPuesto) =>{
+    try {
+    const res = await axios.post(Lugar.backend+'postulacion.php',{
+        id_puesto:idPuesto,
+        // @ts-ignore
+        id_usuario:idUsuario,
+    })
+    } catch (error) {
+    
     };
+};
+
+//Modal Para ver información de las vacantes
+let openModal = false;
+let cargo = null;
+const modalOpen = async (data) => {
+    openModal = false;
+    if(data == 'save'){
+         
+    }
+}
   </script>
 
 <main>
     {#if spinner == true}
          <Spinner />
     {/if}
+    <h1 class="text-info" style="text-shadow: 1px 1px 2px black;">OPORTUNIDADES DE EMPLEO</h1>
+    <p>Siempre estamos buscando personas entusiastas y apasionadas para unirse a nuestro equipo, encuentra la vacante ideal para ti.</p>
     <div class="container">
-        <h1 class="text-info" style="text-shadow: 1px 1px 2px black;">OPORTUNIDADES DE EMPLEO</h1>
-        <p>Siempre estamos buscando personas entusiastas y apasionadas para unirse a nuestro equipo, encuentra la vacante ideal para ti.</p>
         {#each jsonSalida as puesto (puesto.id_puesto)}
-        <div class="grid-container">
-            <div class="card">
+            <div class="card shadow p-3 mb-5 bg-body rounded">
                 <div class="card-header">
-                  Vacante:
+                  Vacante en:
                 </div>
                 <div class="card-body">
                   <h5 class="card-title">{puesto.titulo}</h5>
                   <p class="card-text">{puesto.descripcion}</p>
-                  <p class="card-text">{puesto.fecha_limite}</p>
-                  <button class="btn btn-success">Información</button>
+                  <p class="card-text">{puesto.lugar}</p>
+                  <p class="card-text">{puesto.doctor_solicitante}</p>
+                  <button class="btn btn-success" on:click={() => {openModal = true; cargo = puesto;}}>Información</button>
+                </div>
+                <div class="card-header {getFecha(puesto.diferenciaDias)}" style="color: white;">
+                  Fecha limite: {puesto.fecha_limite}
                 </div>
             </div>
-        </div>
         {/each}
     </div>
     
+    {#if openModal == true}
+        <Modal
+        open={openModal}
+        onClosed={(data) => modalOpen(data)}
+        modalSize="modal-ms"
+        title=" vacante:"
+        saveButtonText="Postularse"
+        closeButtonText="Cerrar"
+        >
+        <h6 class="card-subtitle mt-3 mb-2 text-muted">Requisitos:</h6>
+        <ul class="list-group">
+          {#each cargo.requisitos as requisito}
+              <li class="list-group-item">{requisito.linea}</li>
+          {/each}
+        </ul>
+
+        <h6 class="card-subtitle mt-3 mb-2 text-muted">Ofrecemos:</h6>
+        <ul class="list-group">
+          {#each cargo.ofrecemos as ofrecemo}
+            <li class="list-group-item">{ofrecemo.linea}</li>
+          {/each}
+        </ul>
+
+        <h6 class="card-subtitle mt-3 mb-2 text-muted">Funciones Generales:</h6>
+        <ul class="list-group">
+          {#each cargo.funcionesGenerales as funcionGeneral}
+            <li class="list-group-item">{funcionGeneral.linea}</li>
+          {/each}
+        </ul>
+
+        <h6 class="card-subtitle mt-3 mb-2 text-muted">Habilidades y Conocimientos:</h6>
+        <ul class="list-group">
+          {#each cargo.habilidadesConocimientos as habilidadConocimiento}
+            <li class="list-group-item">{habilidadConocimiento.linea}</li>
+          {/each}
+        </ul>
+        </Modal>
+    {/if}
 
 </main>
 
 <style>
-    .grid-container{
+    /* colores para del semaforo */
+    .verde { background-color: green; }
+    .amarillo { background-color: yellow; }
+    .rojo { background-color: red; }
+
+    .container{
         display:grid;
-        grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr));
+        grid-template-columns: repeat(auto-fill, minmax(25rem, 1fr));
         grid-auto-rows: auto;
         gap: 20px;
     }
     .card{
         border-top: 4px solid #17a2b8;
+        
     }
 </style>
