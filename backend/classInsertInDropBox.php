e1c3d9379f6b46733bedbd932c02108a85743b4d
PMoralesAlvarez <p007bon@gmail.com>
2023-12-08 12:21:46 -0600

    update

--- backend/classInsertInDropBox.php
+++ backend/classInsertInDropBox.php
@@ -20,7 +20,7 @@
             //Ingresamos las llaves de acceso para usar el paquete de DropBox
             $this->key = "1kf5q92ng3m2dwq";
             $this->secret = "c4unus9wecgp7w6";
-            $this->access_key = "sl.BrSkf89MpIPvnRGQhmi-W-F4vikpSEfFMiqboVa9oA2FnK1gIVB0_H7nFui3ES6RPFTz5dmZBmW1aeN1PJlQgHGxfwblVxB57vinJSAD81TLeJvBHSKm9oqNFkCEAY2SjtQOtIs79LbbOXLaEx4q8KE";
+            $this->access_key = "sl.BrUIMpjQ10jm9XJ_vS5HNVS7ng8Mc6SGHf0eUAqckh6jFOiAgIDDy0XKUQmN-3Hbp7mWvRmSsYCLdYlX6VhbwmZo4iFbCqAUzgoulTOTOeHb5ECjhlXHc8O4SpikNFRrENa53_8tp3fCMM2t-h4GNP8";
             $this->app = new DropboxApp($this->key, $this->secret,$this->access_key);
             $this->dropbox = new Dropbox($this->app);
         }
--- src/Administrativo/Videos.svelte
+++ src/Administrativo/Videos.svelte
@@ -1,21 +1,25 @@
 <script>
+    //componentes de js
 import { idVideo } from './../idVideo.js';
 import {verVideo} from '../verVideo'
+//componentes con variables extra
+import Spinner from './../Componentes/Spinner.svelte';
+import Modal from '../Componentes/Modal.svelte';
+//Componentes sin varables extra
 import { onMount } from 'svelte';
 import {push} from 'svelte-spa-router'
 // @ts-nocheck
-
-import Spinner from './../Componentes/Spinner.svelte';
 import axios from "axios";
 import Lugar from "../Lugares";
 
+let spinner = false
 
 const setVerIdVideo = (id_video) => {
     idVideo.update(() => ({
         id_video: id_video,
     }));
 };
-let spinner = false
+//Función para consultar videos
 let infoVideos = [];
 const consultarVideos = async () => {
     try {
@@ -35,19 +39,32 @@ const consultarVideos = async () => {
             console.log(infoVideos);
             
         }
-
         spinner = false;
     } catch (error) {
         
-    }
+    };
 };
   onMount(() =>{
       consultarVideos();
   });
-  //descarga de video
+
+//descarga de video
+const descargaVideo = async () => {
+   
+};
+
+//Función para regresar al menú de postulados
+const Regresar = () =>{
+    push('/Postulados')
+};
+
+//Función para modal de descarga de videos
+let openModal = false;
 let localidad = []
-  const descargaVideo = async () => {
-    try {
+const modalOpen = async (data) =>{
+    openModal =false;
+    if(data == 'save'){
+        try {
         const id_video = $idVideo.id_video; // Obtener el id_video del store
         const res = await axios.post(Lugar.backend + 'downloadVideo.php', { id_video });
         const data = JSON.parse(res.data.d);
@@ -63,14 +80,11 @@ let localidad = []
         } else {
             console.log('No hay id_video disponible para la descarga.');
         }
-    } catch (error) {
-        // Manejar el error según tus necesidades
-    }
-};
-
-const Regresar = () =>{
-
-}
+        } catch (error) {
+            // Manejar el error según tus necesidades
+        };
+    };
+}; 
 </script>
 
 <main>
@@ -93,12 +107,25 @@ const Regresar = () =>{
                 <th >{infoVideo.file_name}</th>
                 <th >{infoVideo.localidad}</th>
                 <th>
-                    <button class="btn btn-success" on:click={descargaVideo}>Descargar Video</button>
+                    <button class="btn btn-success" on:click={() => {openModal = true}}>Descargar Video</button>
                 </th>
               </tr>
             </tbody>
             {/each}
+            <button class="btn btn-success" on:click={Regresar}>Regresar</button>
           </table>
+          {#if openModal ==true}
+             <Modal
+             open={openModal}
+             onClosed={(data) => modalOpen(data)}
+             modalSize="modal-ms"
+             title=" Descargar video"
+             saveButtonText="Descargar"
+             closeButtonText="Cerrar"
+             >
+            <h2>Deseas descarga el video?</h2>
+             </Modal>
+          {/if}
     </div>
 </main>
 
